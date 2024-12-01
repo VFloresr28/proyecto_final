5c33fa4edccbd33525380d5ed9ee96fde88f91f4
Moises324 <151587489+Moises324@users.noreply.github.com>
2024-12-01 09:29:15 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from "./components/Footer";
 import MainPage from "./pages/MainPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import ProductsPage from "./pages/ProductsPage";
+import LoginPage from "./pages/LoginPage";
+import CreateUserPage from "./pages/CreateUserPage";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/products/:id" element={<ProductDetailPage />} />
           <Route path="/products" element={<ProductsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/create-user" element={<CreateUserPage />} />
         </Routes>
       </main>
       <Footer />
