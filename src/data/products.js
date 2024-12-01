7b71480444cfea0785a5d739b44d090e36ead8af
Moises324 <151587489+Moises324@users.noreply.github.com>
2024-12-01 10:05:33 -0400

    images added

--- src/data/products.js
+++ src/data/products.js
@@ -1,16 +1,97 @@
 const products = [
-  { id: 1, name: "Producto 1", description: "Descripción 1", price: "$10.00", image: "https://via.placeholder.com/300x200" },
-  { id: 2, name: "Producto 2", description: "Descripción 2", price: "$20.00", image: "https://via.placeholder.com/300x200" },
-  { id: 3, name: "Producto 3", description: "Descripción 3", price: "$30.00", image: "https://via.placeholder.com/300x200" },
-  { id: 4, name: "Producto 4", description: "Descripción 4", price: "$40.00", image: "https://via.placeholder.com/300x200" },
-  { id: 5, name: "Producto 5", description: "Descripción 5", price: "$50.00", image: "https://via.placeholder.com/300x200" },
-  { id: 6, name: "Producto 6", description: "Descripción 6", price: "$60.00", image: "https://via.placeholder.com/300x200" },
-  { id: 7, name: "Producto 7", description: "Descripción 7", price: "$70.00", image: "https://via.placeholder.com/300x200" },
-  { id: 8, name: "Producto 8", description: "Descripción 8", price: "$80.00", image: "https://via.placeholder.com/300x200" },
-  { id: 9, name: "Producto 9", description: "Descripción 9", price: "$90.00", image: "https://via.placeholder.com/300x200" },
-  { id: 10, name: "Producto 10", description: "Descripción 10", price: "$100.00", image: "https://via.placeholder.com/300x200" },
-  { id: 11, name: "Producto 11", description: "Descripción 11", price: "$110.00", image: "https://via.placeholder.com/300x200" },
-  { id: 12, name: "Producto 12", description: "Descripción 12", price: "$120.00", image: "https://via.placeholder.com/300x200" },
+  {
+    id: 1,
+    name: "Extra-Oversize chill Style",
+    description: "Shein Import",
+    price: "$65.00",
+    image:
+      "https://i.pinimg.com/736x/f0/5e/72/f05e72491b1299315e0095a64074aa7d.jpg",
+  },
+  {
+    id: 2,
+    name: "Extra-Oversize   Aestethic Van-gogh Style",
+    description: "Shein Import",
+    price: "$70.00",
+    image:
+      "https://i.pinimg.com/736x/15/a1/40/15a1405ad30ef3665d1d0ad45f3255b1.jpg",
+  },
+  {
+    id: 3,
+    name: "Cojín de peluche suave y lindo con forma de pata de gato de 60cm para decorar el hogar, la oficina, la sala de estar y el dormitorio",
+    description:
+      "Cojín de peluche suave y lindo con forma de pata de gato de 60cm para decorar el hogar, la oficina, la sala de estar y el dormitorio Multicolor Tela Kids Dolls & Stuffed Toys, size features are:Bust: ,Length: ,Sleeve Length:",
+    price: "$150.00",
+    image:
+      "https://i.pinimg.com/736x/4e/70/b9/4e70b9573b4bb30214e7f98cd998c28c.jpg",
+  },
+  {
+    id: 4,
+    name: "Almohada de gato negro",
+    description:
+      "Almohada de Gato Negro de Peluche de Gato Negro, Almohada Kawaii de Gato Gordo, Peluches, decoración del hogar, Regalo para niños, niñas, cumpleaños, San Valentín, Navidad",
+    price: "$40.00",
+    image:
+      "https://i.pinimg.com/736x/92/b3/c1/92b3c12ec13679df88625b971e3ae8c1.jpg",
+  },
+  {
+    id: 5,
+    name: "Lámpara de pie Smoko 17,7 x Alto 33,4 / Diámetro 45cm x Alto 85cm-púrpura-Enchufe estadounidense",
+    description:
+      "Interesante y única, la lámpara será el toque final que desea para su sala de estar, dormitorio o terraza acristalada. Esta es una pantalla vibrante que ayudará a que las vibraciones de su hogar fluyan. Si tiene alguna pregunta sobre nuestros productos, contáctenos y le responderemos dentro de las 24 horas. Tamaño del producto Tamaño: 45 cm de diámetro x 85 cm de alto / 17,7 x 33,4 de alto Detalles Material: Resina, Metal, Vidrio Fuente de luz: bombilla LED o bombilla Edison Tipo de base de fue",
+    price: "$50.00",
+    image:
+      "https://i.pinimg.com/736x/40/ef/ac/40efac64368c9ebfde7d72806a4ca0cb.jpg",
+  },
+  {
+    id: 6,
+    name: "Lámpara de mesa con decoración de flores luminosas, adornos creativos de estilo simple - Type A / UK",
+    description: "Lámpara de mesas ideal para tu cuarto.",
+    price: "$60.00",
+    image:
+      "https://i.pinimg.com/736x/ac/ca/53/acca53a5e6d29edc15d6944a5a6f58d1.jpg",
+  },
+  {
+    id: 7,
+    name: "Producto 7",
+    description: "Descripción 7",
+    price: "$70.00",
+    image: "https://via.placeholder.com/300x200",
+  },
+  {
+    id: 8,
+    name: "Producto 8",
+    description: "Descripción 8",
+    price: "$80.00",
+    image: "https://via.placeholder.com/300x200",
+  },
+  {
+    id: 9,
+    name: "Producto 9",
+    description: "Descripción 9",
+    price: "$90.00",
+    image: "https://via.placeholder.com/300x200",
+  },
+  {
+    id: 10,
+    name: "Producto 10",
+    description: "Descripción 10",
+    price: "$100.00",
+    image: "https://via.placeholder.com/300x200",
+  },
+  {
+    id: 11,
+    name: "Producto 11",
+    description: "Descripción 11",
+    price: "$110.00",
+    image: "https://via.placeholder.com/300x200",
+  },
+  {
+    id: 12,
+    name: "Producto 12",
+    description: "Descripción 12",
+    price: "$120.00",
+    image: "https://via.placeholder.com/300x200",
+  },
 ];
 
 export default products;
