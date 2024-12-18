38fdc0985e978f878e8b5692f9c5a68bc5e63caf
Valentina Flores <148397734+VFloresr28@users.noreply.github.com>
2024-12-18 20:33:45 -0300

    C
    comit

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,13 +1,33 @@
 import React from "react";
 
 function Footer() {
-    return (
-      <footer className="footer-custom text-center py-3">
-        <div className="container">
-          <p className="mb-0">Derechos reservados a XXXX &copy; {new Date().getFullYear()}</p>
-        </div>
-      </footer>
-    );
-  }
+  return (
+    <footer className="footer-custom">
+      <div className="container text-center py-3">
+        <p>
+          &copy; {new Date().getFullYear()} Derechos reservados a:
+        </p>
+        <p>
+          <a 
+            href="https://github.com/Moises324" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            style={{ marginRight: "15px", color: "#ffffff" }}
+          >
+            <i className="fa-brands fa-github"></i> Moises Gavidia
+          </a>
+          <a 
+            href="https://github.com/VFloresr28?tab=following" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            style={{ color: "#ffffff" }}
+          >
+            <i className="fa-brands fa-github"></i> Valentina Flores
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
 
 export default Footer;
